6390721483ad8099e677fe9692cd3d0a448f284a
LuanHSaueressig <luansaueressig0203@gmail.com>
2025-09-01 17:15:06 -0300

    Desafio sobre classes abstratas concluidogit config --global user.email luansaueressig0203@gmail.com!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4002", modelo: "Modelo 14", imei: "111111", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Pinterest");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "8922", modelo: "Modelo 123", imei: "2222222", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
